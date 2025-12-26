ae662ec6e7a6f4250e258da18c863bc9638b6221
Vladuk225 <dubkapiska@gmail.com>
2025-12-26 11:42:42 +0100

    Refactor TOKEN to use environment variable

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-TOKEN = '7502915190:AAHK7FOhyBobEJG08bYJiUBxYM3bK0yM8Ow'
+import os 
+
+TOKEN = os.getenv('TOKEN')
 bot_username = '@PixelLifeBot'
 start_balance = 1000
 admin_ids = [5960231383, 6902301873]
 owner_id = 5960231383
 ru_doc_link = '...'
 en_doc_link = '...'
+
