2f337d7bf96a5331b91ac4b7f016ed30cc08e639
Vladuk225 <dubkapiska@gmail.com>
2025-12-29 09:27:01 +0100

    Delete keyboards directory

--- keyboards/support.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def finish_report_kb(user_id, _):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=_("support_finish"), callback_data=f'support_finish:{user_id}')],
-        [InlineKeyboardButton(text=_("back"), callback_data=f'faq:{user_id}')]
-    ])
-    return keyboard
--- keyboards/top.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def top_kb(user_id: int, _):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=_("back"), callback_data=f"back_to_info:{user_id}")],
-    ])
-
-    return keyboard
